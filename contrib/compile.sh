984c5fe94ad53b659b15e7aa135f50f8db757650
user <user@debian-arm64.somedomain.tld>
2014-09-18 10:19:50 +0200

    Fixed the paths for libraries to get selected dynamically depending on the Architecture in CMakeLists.txt and compile.sh

--- contrib/compile.sh
+++ contrib/compile.sh
@@ -10,6 +10,6 @@ gdbus-codegen \
     org.freedesktop.Skb.xml \
 && \
 gcc -std=c99 -o service-skb \
-    `pkg-config --cflags --libs gio-2.0 gio-unix-2.0` -I/home/user/Eclipse/include/$ARCH  \
-    -L/home/user/Eclipse/lib/$ARCH  -I$PWD -lreadline -leclipse\
+    `pkg-config --cflags --libs gio-2.0 gio-unix-2.0` -I/usr/local/eclipse/include/$ARCH  \
+    -L/usr/local/eclipse/lib/$ARCH  -I$PWD -lreadline -leclipse\
     service-skb.c Skb.h Skb.c skb_main.c skb_service.c skb_functions.c
