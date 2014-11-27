fea8bb7b6692c77a5b561dbde36b42073f9fec33
pradeepkj <pradeepkj@pradeepkj-OptiPlex-9020>
2014-11-27 18:15:06 +0100

    working numa app

--- sched/numa_app.c
+++ sched/numa_app.c
@@ -1,158 +1,188 @@
+#include "Skb.h"
 #include <numa.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 #include <signal.h>
-
+#include <unistd.h>
 #define OneGB 1024*1024*1024
 int OneMB;
-
 pthread_attr_t attr;
 struct thread_info
 {				/* Used as argument to thread_start() */
   pthread_t thread_id;		/* ID returned by pthread_create() */
   int thread_num;		/* Application-defined thread # */
   char *argv_string;		/* From command-line argument */
-  int chunk_size;
+  size_t chunk_size;
   int node_id;
 };
-
 int running_threads = 0;
-
 pthread_mutex_t running_mutex = PTHREAD_MUTEX_INITIALIZER;
 int num_threads;
 int memory;
-
 struct thread_info *tinfo;
 char *start = NULL;
+
+char L[200];
+char query[200];
+GMainLoop *loop;
+Skb *proxy;
+size_t pages_per_thread;
+
+void
+form_query (char *algo, char *fn_name, int input1, int input2)
+{
+
+  sprintf (query, "[%s], %s(%d,%d,L),write(L)", algo, fn_name, input1,
+	   input2);
+  printf ("query is =========>%s\n", query);
+
+}
 
 void
 thread_start (void *arg)
 {
-
   struct thread_info *thinfo;
   thinfo = arg;
-
   size_t bytes = thinfo->chunk_size;
   thinfo->node_id = 0;
   numa_run_on_node (thinfo->node_id);
-   void *start = (void*)numa_alloc_onnode(bytes,thinfo->node_id);
-
+  printf("number of bytes ==>%u\n",bytes);
+  void *start = (void *) numa_alloc_onnode (bytes, thinfo->node_id);
   if (start == NULL)
     printf ("failed to allocate the memory on node %d\n", thinfo->node_id);
   else
     fflush (stdout);
-
-  memset(start,0,bytes);
-  int *start_addr = (int *) start;
-  int *end_addr = (int *) start + thinfo->chunk_size;
+  memset (start, 0, bytes);
+  char *start_addr =  start;
+  char *end_addr =  start + thinfo->chunk_size;
   printf ("start address is=> %p..end address is ==>%p\n", start_addr,
 	  end_addr);
   fflush (stdout);
-
- int loop;
- for(loop = 0 ; loop < 4 ; loop++){
-
-  start_addr = start;
-  while (start_addr < end_addr)
+  int loop;
+  for (loop = 0; loop < 5; loop++)
     {
-      *start_addr = 1;
-      start_addr++;
-
+      start_addr = start;
+      while (start_addr < end_addr)
+	{
+	  *start_addr++ = 'a';
+	}
+      start_addr = start;
+      while (start_addr < end_addr)
+	{
+	  if (*start_addr++ != 'a');
+	}
     }
-  printf ("write success ful for thread %d\n", tinfo->thread_num);
+  printf ("%d---thread finished read/write test\n", thinfo->thread_num);
   fflush (stdout);
-
-  start_addr = start;
-  while (start_addr < end_addr)
-    {
-      if (*start_addr++ != 1)
-	printf ("Pattern did not match\n");
-    }
-
- }
-  printf ("%d---thread finished read/write test\n", tinfo->thread_num);
-  fflush (stdout);
-  numa_free (start, OneMB);
+  numa_free (start, bytes);
   pthread_mutex_lock (&running_mutex);
   running_threads--;
   pthread_mutex_unlock (&running_mutex);
-
 }
 
-
-void thread_work(int node_id, int num_threads) {
-
+void 
+callback_from_skb_query (GObject * source_object,
+			 GAsyncResult * res, gpointer user_data)
+{
+  GError *error;
+  error = NULL;
+  gchar *qres;
+  GAsyncResult *gasync;
+  gboolean retval;
+  gpointer result;
+  struct thread_info *tinfo;
+  retval = skb_call_query_finish (proxy, &qres, res, NULL);
+  int node_id = 0; 
   if (numa_run_on_node (node_id) < 0)
-    printf ("Failed to run on the  node %d \n", node_id);
-  printf (" run on the  node %d \n", node_id);
-
-
+    printf ("Failed to run on the node %d \n", node_id);
+  printf (" run on the node %d \n", node_id);
   int tnum;
-
+  size_t bytes=pages_per_thread*4096;
   if (num_threads > 0)
     {
-          int chunk_size = OneMB;
-
-      printf ("the chunk size is ==>%d\n", chunk_size);
       int s = pthread_attr_init (&attr);
       if (s != 0)
 	printf ("attr init failed \n");
       for (tnum = 0; tnum < num_threads; tnum++)
 	{
-	  tinfo = calloc (num_threads, sizeof (struct thread_info));
-
-	      if (tinfo == NULL)
-		printf ("calloc failed\n");
-
+	  tinfo = calloc (1, sizeof (struct thread_info));
+	  if (tinfo == NULL)
+	    printf ("calloc failed\n");
 	  tinfo->thread_num = tnum + 1;
-	  tinfo->chunk_size = chunk_size;
+	  tinfo->chunk_size = bytes;
 	  s =
 	    pthread_create (&tinfo->thread_id, &attr,
-			    (void *) &thread_start, &tinfo[tnum]);
+			    (void *) &thread_start, tinfo);
 
 	  pthread_mutex_lock (&running_mutex);
 	  running_threads++;
 	  pthread_mutex_unlock (&running_mutex);
-
 	  if (s != 0)
 	    printf ("pthread_create failed");
 	  else
 	    printf ("thread created successfully\n");
 	}
-
       s = pthread_attr_destroy (&attr);
       if (s != 0)
 	printf ("failed to destroy the struct\n");
-
-
       while (running_threads > 0)
 	{
 	  sleep (1);
 	}
     }
+  g_main_loop_unref (loop);
+  g_main_loop_quit (loop);
+
+
 }
-
-
 
 int
 main (int argc, char *argv[])
 {
-
-
-#if 0
-   if( numa_available() < 0)
-	printf("Numa available\n");
-   else
-	{
-		printf("NUma not avaialale\n");
-		exit(0);
-      }
-
+#if 1
+  if (numa_available () < 0)
+  {
+      printf ("NUma not avaialale\n");
+      exit (0);
+  }
+  else
+    {
+      printf ("Numa available\n");
+    }
 #endif
-  OneMB = atoi(argv[3]);
-  thread_work(atoi(argv[1]),atoi(argv[2]));
+ size_t pages =  sysconf(_SC_AVPHYS_PAGES);
+ size_t page_size = sysconf(_SC_PAGESIZE);
 
+  num_threads = atoi (argv[1]);
+  int s;
+  GError *error;
+  error = NULL;
+  gchar *res;
+  GAsyncResult *gasync;
+  loop = g_main_loop_new (NULL, FALSE);
+  proxy = skb_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION, G_DBUS_PROXY_FLAGS_NONE, "org.freedesktop.Skb",	/* bus name */
+				      "/org/freedesktop/Skb",	/* object */
+				      NULL,	/* GCancellable */
+				      &error);
+
+ int cpu = atoi (argv[4]);
+
+ pages_per_thread = pages / num_threads; 
+  
+ memory = (pages_per_thread*4096*num_threads)/(1024*1024);
+  
+ printf("memory s ==%u\n", memory);
+
+ form_query (argv[2], argv[3], num_threads * cpu, memory);
+
+ skb_call_query (proxy, query, NULL, callback_from_skb_query, NULL);
+
+ g_main_loop_run (loop);
+
+ 
+  //OneMB = atoi (argv[3]);
+//  thread_work (pages_per_thread*page_size,atoi(argv[1]), atoi(argv[2]));
   return 0;
 }
