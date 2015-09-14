5f2671cd27924c7e6011c606ab3cfc8974d7388e
ubuntu <pradeepkiruvale@gmail.com>
2015-09-14 15:33:40 +0200

    added 2nd level scheduler functionality

--- sched/skb_numa/skb_scheduler.c
+++ sched/skb_numa/skb_scheduler.c
@@ -12,12 +12,17 @@ char query[200];
 GMainLoop *loop;
 Skb *proxy;
 char binary[50];
+char **cmd;
+char command[1024];
 
 int cpu, memory, num_threads, l_count;
 char type[20];
+char watch_server[20], next_lvl_server[20];
+int level;
 
-int
-parse_results (gchar * res)
+#if 1
+char *
+parse_node_results (gchar * res)
 {
 
   printf ("the result is --->%s\n", res);
@@ -39,6 +44,63 @@ parse_results (gchar * res)
   return -1;
 }
 
+#endif
+
+  char node_ip[20];
+char *
+parse_results (gchar * res)
+{
+
+  int last_pos = 0;
+  memset(node_ip, 0, 20);
+//  printf ("the result is --->%s\n", res);
+  while (*res != '\0')
+    {
+      if (*res != '[' && *res != ']')
+	{
+	  node_ip[last_pos++] = *res++;
+ //	  printf("char is ==>%c\n",*res);
+	}
+      else
+	res++;
+    }
+ //	  printf("char is ==>%c\n", node_ip);
+  return node_ip;
+}
+
+void write_to_nxt_level_etcd( char *node)
+{
+
+  
+  //printf("the node is ======> %s\n",node);
+  char* nxt_lvl_node = parse_results(node); 
+  //printf("the node is ======> %s\n",nxt_lvl_node);
+
+  create_etcd_session (nxt_lvl_node);
+  do_set (NULL, NULL, NULL, NULL, "taskqueue"); 
+  do_set ("taskqueue/task", command , NULL, NULL, NULL);	//write the task to nxt level node
+  close_etcd_session ();
+
+}
+
+char *
+watch_etcd_change (char *read_server, char *direct)
+{
+
+  int chflag;
+  while (1)
+    {
+      create_etcd_session (read_server);
+      chflag = do_watch (read_server, direct);
+      if (chflag == 1)
+	{
+  		close_etcd_session ();
+		return do_get(direct);
+	}
+      sleep (1);
+    }
+}
+
 void 
 callback_from_skb_query (GObject * source_object,
 			 GAsyncResult * res, gpointer user_data)
@@ -52,15 +114,21 @@ callback_from_skb_query (GObject * source_object,
   gpointer result;
   struct thread_info *tinfo;
   retval = skb_call_query_finish (proxy, &qres, res, NULL);
-  printf("the result is ==>%s\n", qres);
-  exit(1);
-  int node_id = parse_results(qres); 
   
-  char command[1024];
-  sprintf(command,"numactl --cpunodebind %d --membind %d %s %d %d",node_id, node_id, binary, num_threads, l_count);
+  if(!level)
+  {
+  	sprintf(command,"binary=%s memory=%d cpu=%d", binary, memory, cpu);
+  	write_to_nxt_level_etcd(qres);
+  }
+  else //@node level query for devices and schedule
+  {
+	int node_id = parse_node_results(qres);
+ 	sprintf(command,"numactl --cpunodebind %d --membind %d %s",node_id, node_id, binary);
+        system( command );  
+  }
 
-  printf("the command is == %s\n",command);
-  system( command );  
+  //printf("the command is == %s\n",*cmd);
+  //printf("the command is == %s\n",nxt_lvl_node);
 
   g_main_loop_unref (loop);
   g_main_loop_quit (loop);
@@ -73,7 +141,7 @@ form_query (char *ty, int cpu, int memory)
 
   //sprintf (query, "[test_algo], get_free_numa_node(%d,%d,L),write(L)",memory, cpu);
   sprintf (query, "[test_algo], get_all_sysinfo(%s,%d,%d,L),write(L)",ty,cpu,memory);
-  printf ("query is =========>%s\n", query);
+  //printf ("query is =========>%s\n", query);
 
 }
 
@@ -81,10 +149,10 @@ void parse_inputs(int numargs, char**argv)
 {
 
   int i;
-  printf("num args===>%d\n", numargs);
+  //printf("num args===>%d\n", numargs);
   for(i = 1 ; i < numargs ; i++)
   {
-    printf("the argvis ==>%s\n",argv[i]);
+    //printf("the argvis ==>%s\n",argv[i]);
 #if 1
     int pos = 0;
     char *rc = NULL;
@@ -93,17 +161,17 @@ void parse_inputs(int numargs, char**argv)
     if(!strcmp("memory", rc )){
     	rc = strtok(NULL, " \0");
 	memory = atoi(rc);
-	printf("mem is ==>%d\n",memory);
+	//printf("mem is ==>%d\n",memory);
     }
    else if(!strcmp("cpu", rc )){
     	rc = strtok(NULL, " \0");
 	cpu  = atoi(rc);
-	printf("cpu is ==>%d\n",cpu);
+	//printf("cpu is ==>%d\n",cpu);
     }
    else if(!strcmp("binary", rc )){
     	rc = strtok(NULL, " \0");
 	sprintf (binary,"%s", rc);
-	printf("binary is ==>%s\n",cpu);
+	//printf("binary is ==>%s\n",cpu);
     }
     else if(!strcmp("type", rc )){
     	rc = strtok(NULL, " \0");
@@ -117,12 +185,24 @@ void parse_inputs(int numargs, char**argv)
     	rc = strtok(NULL, " \0");
 	l_count = atoi(rc);
     }
+   else if(!strcmp("watchserver", rc )){
+    	rc = strtok(NULL, " \0");
+	sprintf (watch_server,"%s", rc);
+    }
+   else if(!strcmp("nextserver", rc )){
+    	rc = strtok(NULL, " \0");
+	sprintf (next_lvl_server,"%s", rc);
+    }
+  else if(!strcmp("level", rc )){
+    	rc = strtok(NULL, " \0");
+	level = atoi(rc);
+    }
 
     //printf("the %s is %d\n",rc[0],atoi(rc[1]));
 #endif
   }	
 
-    printf("the %d cpu %d memory %d threds \n",cpu, memory, num_threads);
+    //printf("the %d cpu %d memory %d threds \n",cpu, memory, num_threads);
 
 }
 
@@ -152,16 +232,19 @@ main (int argc, char *argv[])
 				      "/org/freedesktop/Skb",	/* object */
 				      NULL,	/* GCancellable */
 				      &error);
- 
  parse_inputs(argc, argv);
-  
- //strcpy(binary, argv[1]);
 
- printf(" Application asked for %u MB of memory and %u percentage of CPU\n", memory,cpu);
+ if(level)
+	 while (1) {
+		char *cmd = watch_etcd_change(watch_server,"taskqueue/task");
+        	parse_inputs(3, &cmd);
+	 	printf(" Application asked for %u MB of memory and %u percentage of CPU\n", memory,cpu);
+	 	form_query (type, cpu, memory);
+	 	skb_call_query (proxy, query, NULL, callback_from_skb_query, NULL);
+	}
 
- form_query (type, cpu, memory);
-
- skb_call_query (proxy, query, NULL, callback_from_skb_query, NULL);
+	form_query (type, cpu, memory);
+ 	skb_call_query (proxy, query, NULL, callback_from_skb_query, NULL);
 
  g_main_loop_run (loop);
  
